544a5071dafbfe7011478e1920285b0432cd0dea
Bartu Atabek <batabek@infinitus.cf>
2021-01-25 07:33:11 +0300

    Update config for deployment.

--- server/config.js
+++ server/config.js
@@ -4,10 +4,10 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const config = {
-    client_id: '00d4d68cef3af940c8ef',
-    redirect_uri: 'http://localhost:3000/login',
-    client_secret: '813fc7970d57f825ac7de572d55bd4c0108d8a3d',
-    proxy_url: 'http://localhost:5000/authenticate'
+    client_id: '836f35dd5231a7d99587',
+    redirect_uri: 'https://bartuatabek.github.io/git-dash/login',
+    client_secret: 'c27aca8e0c3000720c6e7f4514e1b05a846564fd',
+    proxy_url: 'https://bartuatabek.github.io/git-dash:5000/authenticate'
 };
 
 const envVarsSchema = Joi.object({
